4bc0ceacb85001a446d8250d9d3da9fa0e21b6f1
Natchanon A. <natchanon_347@hotmail.com>
2017-11-11 15:21:38 +0700

    GameLogic

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -1,10 +1,14 @@
 package logic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameLogic {
 	private List<Enemy> enemyList;
 	private List<Bullet> bulletList;
 	
-
+	public GameLogic() {
+		enemyList = new ArrayList<>();
+		bulletList = new ArrayList<>();
+	}
 }
