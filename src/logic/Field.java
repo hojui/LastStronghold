950fc95da564aaa50342f506d35363277a8276ba
hojui <siwa.boonpun@gmail.com>
2017-11-11 14:37:18 +0700

    Add Field

---
+++ src/logic/Field.java
@@ -0,0 +1,31 @@
+package logic;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import share.IRenderable;
+
+public class Field implements IRenderable{
+
+	public Field() {
+		
+	}
+	
+	@Override
+	public void draw(GraphicsContext gc) {
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, 800, 450);
+		gc.setFill(Color.WHITE);
+		for (int i = 0 ; i < 10 ; i++) {
+			int randomX = (int) (Math.random()*800 +1);
+			int randomY = (int) (Math.random()*450 +1);
+			gc.fillOval(randomX,randomY,5,5);
+		}
+		for (int i = 0 ; i < 5 ; i++) {
+			int randomX = (int) (Math.random()*800 +1);
+			int randomY = (int) (Math.random()*450 +1);
+			gc.fillOval(randomX,randomY,10,10);
+		}
+		
+	}
+
+}
