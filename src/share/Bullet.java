48816299f162192fda5598b2bd0c0fbc879f6c1f
Natchanon A <natchanon_347@hotmail.com>
2017-11-11 13:27:15 +0700

    Add Bullet class

--- src/share/Bullet.java
+++ src/share/Bullet.java
@@ -1,5 +1,13 @@
 package share;
 
-public class Bullet {
+import javafx.scene.canvas.GraphicsContext;
 
+public class Bullet extends Sprite implements IRenderable {
+
+	@Override
+	public void draw(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
